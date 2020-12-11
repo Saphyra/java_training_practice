67a60b5d94e48a328fc62d09ab4ff98e77b9a7d6
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-12-11 18:53:06 +0100

    Requests

--- src/main/java/com/github/training/controller/request/RegisterAccountRequest.java
+++ src/main/java/com/github/training/controller/request/RegisterAccountRequest.java
@@ -2,12 +2,14 @@ package com.github.training.controller.request;
 
 import lombok.Data;
 
-
 @Data
-public class RegisterUserRequest {
+public class RegisterAccountRequest {
     private String username;
     private String password;
     private String email;
     private String name;
-
+    private AddressRequest address;
+    private String phoneNumber;
+    private String birthDate;
+    private String currency;
 }
