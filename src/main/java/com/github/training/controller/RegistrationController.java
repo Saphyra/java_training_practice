2622f1ae1d33bf97d6dc3914e8843158411eec4f
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-12-13 17:04:43 +0100

    RegistrationController

--- src/main/java/com/github/training/controller/RegistrationController.java
+++ src/main/java/com/github/training/controller/RegistrationController.java
@@ -2,9 +2,7 @@ package com.github.training.controller;
 
 import com.github.training.controller.request.RegisterAccountRequest;
 import com.github.training.database.Account;
-import com.github.training.database.AccountRepository;
 import com.github.training.database.ShippingInformation;
-import com.github.training.database.ShippingInformationRepository;
 import com.github.training.validation.RegisterAccountRequestValidation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,16 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class RegistrationController {
     private final RegisterAccountRequestValidation registerAccountRequestValidation;
-    private final AccountRepository accountRepository;
-    private final ShippingInformationRepository shippingInformationRepository;
-
 
     @PostMapping("/registration")
     public void registration(@RequestBody RegisterAccountRequest registerAccountRequest){
@@ -31,10 +24,8 @@ public class RegistrationController {
             log.error("Nem megfelelő adatok");
             throw new IllegalArgumentException("Nem megfelelő adatok");
         }
-        UUID userId = UUID.randomUUID();
         Account account = new Account();
 
-        account.setUserId(userId);
         account.setUsername(registerAccountRequest.getUsername());
         account.setPassword(registerAccountRequest.getPassword());
         account.setEmail(registerAccountRequest.getEmail());
@@ -42,7 +33,6 @@ public class RegistrationController {
 
         ShippingInformation shippingInformation = new ShippingInformation();
 
-        shippingInformation.setUserId(userId);
         shippingInformation.setName(registerAccountRequest.getName());
         shippingInformation.setCountry(registerAccountRequest.getAddress().getCountry());
         shippingInformation.setZipCode(registerAccountRequest.getAddress().getZipCode());
@@ -53,9 +43,5 @@ public class RegistrationController {
         shippingInformation.setDoor(registerAccountRequest.getAddress().getDoor());
         shippingInformation.setPhoneNumber(registerAccountRequest.getPhoneNumber());
         shippingInformation.setBirthDate(registerAccountRequest.getBirthDate());
-
-        accountRepository.save(account);
-        shippingInformationRepository.save(shippingInformation);
     }
-
 }
