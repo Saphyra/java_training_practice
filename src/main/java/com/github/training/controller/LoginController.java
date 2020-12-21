add21e6fcadf0a43d6b7a88c2950f27698ff543d
LeserHUN <bszab100@gmail.com>
2020-12-21 17:00:19 +0100

    log be

---
+++ src/main/java/com/github/training/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.github.training.controller;
+
+import com.github.training.controller.request.LoginAccountRequest;
+import com.github.training.database.Account;
+import com.github.training.database.AccountRepository;
+import com.github.training.validation.LoginAccountRequestValidation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Objects.isNull;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class LoginController {
+    private final AccountRepository accountRepository;
+    private final LoginAccountRequestValidation loginAccountRequestValidation;
+
+
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginAccountRequest loginAccountRequest){
+        log.info("Login request test arrived: {}", loginAccountRequest);
+        boolean logAccVal = loginAccountRequestValidation.loginAccountValidation(loginAccountRequest);
+        if (!logAccVal) {
+            log.error("Nem megfelelő adatok");
+            throw new IllegalArgumentException("Nem megfelelő adatok");
+        }
+        Account account = accountRepository.findByEmail(loginAccountRequest.getEmail());
+
+        if (isNull(account)){
+            //TODO
+        }
+        if (!account.getPassword().equals(loginAccountRequest.getPassword())){
+            //toDo
+        }
+
+    }
+}
---
+++ src/main/java/com/github/training/service/LoginSessionService.java
@@ -0,0 +1,12 @@
+package com.github.training.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+
+public class LoginSessionService {
+
+
+}
