17764bbd029c7cfb792f5a6f14a66bf9b4d60bd2
DiamondPRO02 <kovacsdanielmark@gmail.com>
2020-12-21 17:31:34 +0100

    Login

--- src/main/java/com/github/training/controller/LoginController.java
+++ src/main/java/com/github/training/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.github.training.controller;
 import com.github.training.controller.request.LoginAccountRequest;
 import com.github.training.database.Account;
 import com.github.training.database.AccountRepository;
+import com.github.training.database.LoginSession;
+import com.github.training.service.LoginSessionService;
 import com.github.training.validation.LoginAccountRequestValidation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +20,7 @@ import static java.util.Objects.isNull;
 public class LoginController {
     private final AccountRepository accountRepository;
     private final LoginAccountRequestValidation loginAccountRequestValidation;
-
-
+    private final LoginSessionService loginSessionService;
 
     @PostMapping("/login")
     public void login(@RequestBody LoginAccountRequest loginAccountRequest){
@@ -37,6 +38,7 @@ public class LoginController {
         if (!account.getPassword().equals(loginAccountRequest.getPassword())){
             //toDo
         }
+        LoginSession loginSession = loginSessionService.createSession(account.getUserId(), loginAccountRequest.isRemember());
 
     }
 }
