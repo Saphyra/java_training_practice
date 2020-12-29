83f85ef089c56bcc71ed56233c93bd1e3dcc633c
bondabear@gmail.com <Saphy371321>
2020-12-29 16:23:50 +0100

    Clean up expired loginSessions

--- src/main/java/com/github/training/database/LoginSessionRepository.java
+++ src/main/java/com/github/training/database/LoginSessionRepository.java
@@ -2,8 +2,9 @@ package com.github.training.database;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface LoginSessionRepository extends CrudRepository<LoginSession, UUID> {
-
+    void deleteByRememberAndLastAccessBefore(boolean remember, LocalDateTime expiration);
 }
